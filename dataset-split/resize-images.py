1c336ddddeb962029c60f04cc490d2ab2c8dd543
JaMixon <99218198+JaMixon@users.noreply.github.com>
2024-11-21 09:52:22 -0600

    Update resize-images.py

--- dataset-split/resize-images.py
+++ dataset-split/resize-images.py
@@ -4,8 +4,8 @@ import numpy as np
 from tqdm import tqdm
 import cv2
 
-data_path = '/home/jmix/code/bsm-ds-augment/data/train'
-dest_data_path = '/home/jmix/code/bsm-ds-augment/data/resize/train'
+data_path = # enter path to training data folder 
+dest_data_path = # enter path to training data destination folder
 mask = '.bmp'
 
 directories = [directory for directory in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, directory))]
@@ -18,7 +18,7 @@ for directory in directories:
     print(f'Number of files in the directory {directory}: {len(files)}')
 
     for file in tqdm(files):
-        # print(f'Processing file: {file}')
+        
         if file.endswith(mask):
             img = cv2.imread(os.path.join(data_path, directory, file))
             img = cv2.resize(img, (768, 768))
